0f6eb5bf4891b53a198ff0b00c0ace4de71d32c3
Emon8794 <emonabujafor@gmail.com>
2026-02-26 17:38:25 +0600

    feat: initialize task management dashboard with React, Vite, and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <div className="bg-linear-to-r from-primary to-primary-dark text-white p-6 rounded-2xl">
+        
+      </div>
+    </div>
+  );
+};
+
+export default App;
